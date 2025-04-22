169bddaf4df0bb73f9f26d57693eed7201d23dba
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2025-04-22 15:57:30 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -36,7 +36,7 @@ fi
 
 echo "Applying stow for dotfiles..."
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-chmod +x ${SCRIPT_DIR}/vm/*
+chmod +x ${SCRIPT_DIR}/vm/.local/bin/*
 
 stow -v -t ~ -d $SCRIPT_DIR tmux kitty nvim vm zsh
 
