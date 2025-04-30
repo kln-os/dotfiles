a858ecd7e8b7201460049337270342240e3ce6a7
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-04-30 20:14:56 +0700

    update

--- main.sh
+++ main.sh
@@ -3,9 +3,9 @@ echo "=== Starting installation of dotfiles ==="
 echo "Applying stow for dotfiles..."
 rm -rf ~/.zshrc
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-chmod +x ${SCRIPT_DIR}/vm/.local/bin/*
+chmod +x ${SCRIPT_DIR}/visudo/.local/bin/*
 mkdir -p ~/.local/bin 
 
-stow -v -t ~ -d $SCRIPT_DIR tmux kitty nvim vm zsh
+stow -v -t ~ -d $SCRIPT_DIR tmux kitty nvim visudo zsh
 
 echo "-----------------------------------------"
