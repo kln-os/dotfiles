7f48f826473ab30b7a0303360e88a3d618f502b7
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-06 18:26:07 +0700

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 echo "=== Starting installation of dotfiles ==="
 
 echo "Applying stow for dotfiles..."
-# rm -rf ~/.zshrc ~/.config/atuin/
+rm -rf ~/.zshrc ~/.config/atuin/ ~/.config/kitty
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 chmod +x ${SCRIPT_DIR}/visudo/.local/bin/*
 mkdir -p ~/.local/bin 
@@ -9,7 +9,7 @@ mkdir -p ~/.local/bin
 for package in tmux kitty nvim visudo zsh atuin; do
     if [ -d "${SCRIPT_DIR}/${package}" ]; then
         echo "Stowing ${package}..."
-        stow -v -t ~ -d $SCRIPT_DIR $package # --adopt
+        stow --adopt -v -t ~ -d $SCRIPT_DIR $package 
     else
         echo "Warning: Package ${package} not found in ${SCRIPT_DIR}"
     fi
